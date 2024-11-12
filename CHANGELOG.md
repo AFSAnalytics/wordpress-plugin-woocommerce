8709c79e5fda83721075928b3934fbf7fc6972ae
JM-AFSAnalytics <jm@afsanalytics.com>
2024-11-12 18:17:33 +0100

    v2.19

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## 2.1.9 - 2024-11-12
+
+- Wordpress 6.6 support.
+- WooCommerce 9.3 support.
+
 ## 2.1.8 - 2023-08-24
 
 - Wordpress 6.3 support.
--- afs-analytics-woocommerce.php
+++ afs-analytics-woocommerce.php
@@ -4,13 +4,13 @@
   Plugin Name: AFS Analytics for WooCommerce
   Plugin URI: https://www.afsanalytics.com/
   Description: Advanced eCommerce Analytics solution. Grow your online business by measuring user satisfaction and site efficiency.
-  Version: 2.18
+  Version: 2.19
   Author: AFS Analytics
   Author URI: https://www.afsanalytics.com/
   Text Domain: afsanalytics
   Domain Path: /languages
   WC requires at least: 1.4.1
-  WC tested up to: 8.0.2
+  WC tested up to: 9.3.3
  */
 
 
@@ -36,7 +36,7 @@ if ( ! class_exists( 'AFSA_Core_Stats_Plugin' ) ) :
 
 
 	define( 'AFSA_DEBUG_MODE', false );
-	define( 'AFSA_MODULE_VERSION', '2.0.18' );
+	define( 'AFSA_MODULE_VERSION', '2.0.19' );
 
 	class AFSA_Core_Stats_Plugin {
 
